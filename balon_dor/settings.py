7a37171a76514c9ddc70562beb46351b9c5f7df3
Mhaxe <mxwllhayibor@gmail.com>
2026-03-16 14:49:32 +0000

    init vercel deploy

--- balon_dor/settings.py
+++ balon_dor/settings.py
@@ -41,7 +41,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-1la@k+b@zkl4nk(!lk8inat1mi1rp$m-gs1n2w-if#_1ovlt9s"
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
